583040b6f749cb02c663bb76e5d22b3285e7edab
JohnMukendi <mukendijohn937@gmail.com>
2022-11-21 15:34:43 +0200

    pushing to deploy

--- pages/shows-episodes/[id].js
+++ pages/shows-episodes/[id].js
@@ -1,11 +1,10 @@
 import React from "react";
 import { useEffect, useState, useContext } from "react";
 import { USER_CONTEXT } from "../../context/MainContext";
-import Aos from "aos";
-import "aos/dist/aos.css";
+// import Aos from "aos";
+// import "aos/dist/aos.css";
 import Box from "@mui/material/Box";
 import TabsEpisode from "../../components/shows/TabsEpsiode";
-import axios from "axios";
 import { ShowsContext } from "../../context/ShowContext";
 import { useRouter } from "next/router";
 import { Typography } from "@mui/material";
@@ -15,18 +14,14 @@ const ShowsEpisodes = () => {
   const router = useRouter();
   const title = router.query.id;
   console.log(router, { title });
-  const {show,getShow} = useContext(ShowsContext)
+  const { show, getShow } = useContext(ShowsContext);
   console.log({ show });
 
   useEffect(() => {
-    Aos.init({ duration: 2000 });
-    getShow(title)
+    // Aos.init({ duration: 2000 });
+    getShow(title);
   }, []);
 
-
-  const { showsDetails, setShowsDetails } = useContext(USER_CONTEXT);
-
-  
   return (
     <Box
       style={{ background: "#111", height: "100vh", flexDirection: "column" }}
@@ -44,10 +39,10 @@ const ShowsEpisodes = () => {
       ></Box>
       <div>
         {show.showJson.episodes.length === 0 ? (
-          <Box sx={{textAlign : 'center',padding : '24px 0px'}}>
-            <Typography className="active-tv-font" color='#eee' variant="h3">
-            This Show has no episdoes yet
-          </Typography>
+          <Box sx={{ textAlign: "center", padding: "24px 0px" }}>
+            <Typography className="active-tv-font" color="#eee" variant="h3">
+              This Show has no episdoes yet
+            </Typography>
           </Box>
         ) : (
           <TabsEpisode
